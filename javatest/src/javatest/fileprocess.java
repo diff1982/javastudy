2a565ec8066b8a502f079e8a31aca8fc5e31b3b3
diff1982 <43110873@qq.com>
2017-08-04 00:35:33 +0800

    file process for json

---
+++ javatest/src/javatest/fileprocess.java
@@ -0,0 +1,48 @@
+package javatest;
+import java.util.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.*;
+
+
+public class fileprocess {
+	int[][] dataFormatS= {{0,1},{2,3},{20,24},{25,27},{49,56},{59,66}};
+	int[][] dataFormatR= {{0,1},{2,3},{20,24},{25,27},{49,56},{59,66}};
+	BufferedReader reader = null;
+	List<List<String>> res= null;
+	public List<List<String>> sfilein(String filename) {
+		File sfile = new File(filename);
+		try {
+		reader = new BufferedReader(new FileReader(sfile));
+		String tempString = null;
+		int line =1;
+		while((tempString = reader.readLine())!=null) {			
+			List<String> temp = null;
+			String filetype = tempString.substring(0, 1);
+			String lineNo = tempString.substring(2,3);
+			String pointNo = tempString.substring(19,24);
+			String xCodinate = tempString.substring(48,56);
+			String yCodinate = tempString.substring(58,66);
+			temp.add(filetype);
+			temp.add(lineNo);
+			temp.add(pointNo);
+			temp.add(xCodinate);
+			temp.add(yCodinate);
+			res.add(temp);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(reader!=null)
+				try {
+					reader.close();
+					}catch(IOException e1) {
+					}
+		}
+		return res;
+	}
+}
